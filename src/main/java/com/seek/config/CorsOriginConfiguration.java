da5589dc07dae3b6af5fa1e5304d820a7082bb50
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2025-03-06 14:50:07 -0500

    feat: swagger

--- src/main/java/com/seek/config/CorsOriginConfiguration.java
+++ src/main/java/com/seek/config/CorsOriginConfiguration.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsOriginConfiguration implements WebMvcConfigurer {
   private static final String LOCALHOST = "http://localhost:4200";
+  private static final String SEEK_WEB = "http://seek.chichinacds.com.co/";
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins(LOCALHOST)
+            .allowedOrigins(LOCALHOST, SEEK_WEB)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
---
+++ src/main/java/com/seek/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package com.seek.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "SEEK",
+                        email = "Estarlin.elv@gmail.com",
+                        url = "http://seek.chichinacds.com.co/"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - VMC",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "http://seek.chichinacds.com.co/"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8087"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://seek.chichinacds.com.co/"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
